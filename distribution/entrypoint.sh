b6b117cfb008e7a3fb9fcebe869dabd284cced5b
Varad <varad.ahirwadkar1@ibm.com>
2026-04-01 19:47:37 +0530

    disable milvus-lite on ppc64le architecture
    Signed-off-by: Varad <varad.ahirwadkar1@ibm.com>

--- distribution/entrypoint.sh
+++ distribution/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 
+# Milvus Lite is not supported on ppc64le, so we disable it for Power arch
+ARCH=$(uname -m)
+if [ "$ARCH" = "ppc64le" ]; then
+  unset ENABLE_MILVUS_LITE
+  echo "Architecture: $ARCH - Milvus Lite disabled (not supported on ppc64le)"
+elif [ -z "$ENABLE_MILVUS_LITE" ]; then
+  export ENABLE_MILVUS_LITE=true
+  echo "Architecture: $ARCH - Enabling Milvus Lite by default (ENABLE_MILVUS_LITE=true)"
+else
+  echo "Architecture: $ARCH - Using existing ENABLE_MILVUS_LITE=$ENABLE_MILVUS_LITE"
+fi
+
 # Resolve config path
 if [ -n "$RUN_CONFIG_PATH" ] && [ -f "$RUN_CONFIG_PATH" ]; then
   CONFIG="$RUN_CONFIG_PATH"
